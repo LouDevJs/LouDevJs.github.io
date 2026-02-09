94e78e475025a8c61209dc6f9e0c72cb6daa5533
José Luis Gómez <58857705+LouDevJs@users.noreply.github.com>
2026-02-09 10:58:03 -0400

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hola 👋🏻')
+const nmbr = document.querySelector('#nmbr');
+
+window.onload = (function() {
+  nmbr.animate([
+    { opacity: 0, transform: 'scale(0)' },
+    { opacity: 1, transform: 'scale(1)' }
+  ], {
+    duration: 1500,
+    easing: 'ease-in',
+    fill: 'forwards'
+  });
+})();
